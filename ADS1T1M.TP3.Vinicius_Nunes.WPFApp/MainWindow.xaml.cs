4ca407870eb1d6e03f63ad964295917e73af5376
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-04-05 23:49:54 -0300

    Criação do Layout do WPF e funções do btn

--- ADS1T1M.TP3.Vinicius_Nunes.WPFApp/MainWindow.xaml.cs
+++ ADS1T1M.TP3.Vinicius_Nunes.WPFApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ADS1T1M.TP3.Vinicius_Nunes.Domain.Entities;
+using ADS1T1M.TP3.Vinicius_Nunes.Infra.Data.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,77 @@ namespace ADS1T1M.TP3.Vinicius_Nunes.WPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscarMatricula_Click(object sender, RoutedEventArgs e)
+        {
+            string matricula = txtMatriculaBusca.Text;
+            AcessarDocumento dados = new AcessarDocumento();
+            Aluno aluno = dados.retornarAluno(matricula);
+
+            if (aluno == null)
+            {
+                txtStatus.Text = "Aluno não encontrado";
+                alteraCorStatus("inexistente");
+            }
+            else
+            {
+                validarStatusAluno(aluno);
+            }
+        }
+
+        private void exibirInfoAluno(Aluno aluno)
+        {
+            txtNome.Text = aluno.Nome;
+            txtMatricula.Text = aluno.Matricula;
+            txtDataDeNascimento.Text = aluno.dataDeNascimento.ToString("dd/MM/yyyy");
+            txtCpf.Text = aluno.Cpf;
+        }
+
+        private void validarStatusAluno(Aluno aluno)
+        {
+            if (aluno.Ativo)
+            {
+                txtStatus.Text = "Aluno liberado";
+                exibirInfoAluno(aluno);
+                alteraCorStatus("liberado");
+            }
+            else
+            {
+                txtStatus.Text = "Aluno suspenso";
+                exibirInfoAluno(aluno);
+                alteraCorStatus("suspenso");
+            }
+        }
+
+        private void alteraCorStatus(string status)
+        {
+            txtStatus.Visibility = Visibility.Visible;
+
+            switch (status)
+            {
+                case "inexistente":
+                    txtStatus.Background = Brushes.Blue;
+                    break;
+                case "liberado":
+                    txtStatus.Background = Brushes.Green;
+                    break;
+                case "suspenso":
+                    txtStatus.Background = Brushes.Red;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void matricula_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtNome.Text = "";
+            txtMatricula.Text = "";
+            txtCpf.Text = "";
+            txtDataDeNascimento.Text = "";
+
+            txtStatus.Visibility = Visibility.Hidden;
         }
     }
 }
